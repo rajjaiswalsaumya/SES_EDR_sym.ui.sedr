3ad404caf1b66bd57379521cf474ed7dda19da62
rajjaiswalsaumya <rajjaiswalsaumya@gmail.com>
2021-01-10 22:12:20 +0530

    Add support for stylelint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sedr-app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sedr-app';
